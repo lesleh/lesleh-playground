7ab4238acf7719c3344649d447f69db5ed6bda30
Leslie Hoare <iam@lesleh.co.uk>
2022-10-26 17:54:38 +0100

    Fix props PROPerly this time

--- components/Link/Link.tsx
+++ components/Link/Link.tsx
@@ -1,20 +1,18 @@
 import clsx from "clsx";
 import NextLink from "next/link";
+import type { ComponentProps } from "react";
 import { LinkProps } from "./types";
 
+function isInternalLink(href: ComponentProps<typeof NextLink>["href"]) {
+  return typeof href === "string" && href.startsWith("/");
+}
+
 export function Link({ href, className, ...props }: LinkProps) {
-  const isInternalLink = href && (href.startsWith("/") || href.startsWith("#"));
   const classNames = clsx("text-blue-500 underline", className);
 
-  const Component = isInternalLink ? NextLink : "a";
-
-  return (
-    <Component
-      className={classNames}
-      href={href}
-      target="_blank"
-      rel="noopener noreferrer"
-      {...props}
-    />
+  return isInternalLink(href) ? (
+    <NextLink href={href} className={classNames} {...props} />
+  ) : (
+    <a href={href.toString()} className={classNames} {...props} />
   );
 }
--- components/Link/types.ts
+++ components/Link/types.ts
@@ -1,1 +1,4 @@
-export type LinkProps = React.AnchorHTMLAttributes<HTMLAnchorElement>;
+import type NextLink from "next/link";
+import type { ComponentProps } from "react";
+
+export type LinkProps = ComponentProps<typeof NextLink>;
