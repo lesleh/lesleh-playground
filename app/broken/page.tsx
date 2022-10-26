088a9e83f363e0727211057c4b862422c9404d3e
Leslie Hoare <iam@lesleh.co.uk>
2022-10-26 20:18:13 +0100

    Checking

---
+++ app/broken/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function BrokenPage() {
+  return (
+    <div>
+      <h1>Broken Page</h1>
+    </div>
+  );
+}
---
+++ app/working/page.tsx
@@ -0,0 +1,7 @@
+export default function WorkingPage() {
+  return (
+    <div>
+      <h1>Working Page</h1>
+    </div>
+  );
+}
