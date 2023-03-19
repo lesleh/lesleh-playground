f8def54ab9ccdc4498a1220a25331ad8b10fac6c
Leslie Hoare <iam@lesleh.co.uk>
2023-03-19 14:37:26 +0000

    Move to src directory

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,7 @@ const plugin = require("tailwindcss/plugin");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/{app,pages,components}/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
