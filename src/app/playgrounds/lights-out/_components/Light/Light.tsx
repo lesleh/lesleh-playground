6c675c9e710cf0a249bf2d5ece92572629b16aa9
Leslie Hoare <iam@lesleh.co.uk>
2024-12-30 19:58:20 +0000

    3d

--- src/app/playgrounds/lights-out/_components/Light/Light.tsx
+++ src/app/playgrounds/lights-out/_components/Light/Light.tsx
@@ -10,14 +10,23 @@ interface LightProps {
 export function Light({ onClick, isOn, isPlaying, className }: LightProps) {
   return (
     <button
-      className={clsx("h-16 w-16 relative overflow-hidden", className, {
-        "bg-red-500": isOn,
-        "bg-gray-500": !isOn,
-        "cursor-pointer": isPlaying,
-      })}
+      className={clsx(
+        "h-16 w-16 relative overflow-hidden transition-transform active:scale-95 active:brightness-75",
+        className,
+        {
+          "bg-red-500": isOn,
+          "bg-gray-500": !isOn,
+          "cursor-pointer": isPlaying,
+        },
+      )}
       onClick={onClick}
     >
-      <div className="absolute inset-x-0 top-0 h-1/2 bg-gradient-to-b from-white/30 to-transparent pointer-events-none" />
+      <div
+        className={clsx(
+          "absolute inset-x-0 top-0 h-1/2 bg-gradient-to-b from-white/30 to-transparent pointer-events-none transition-opacity",
+          "active:opacity-50",
+        )}
+      />
     </button>
   );
 }
