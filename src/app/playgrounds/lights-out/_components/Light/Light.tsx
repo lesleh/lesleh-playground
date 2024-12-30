9ef00a3d3b9f6519de88772d291c1dfd4f33e1ac
Leslie Hoare <iam@lesleh.co.uk>
2024-12-30 20:08:08 +0000

    Fix styling while not playing

--- src/app/playgrounds/lights-out/_components/Light/Light.tsx
+++ src/app/playgrounds/lights-out/_components/Light/Light.tsx
@@ -10,21 +10,25 @@ interface LightProps {
 export function Light({ onClick, isOn, isPlaying, className }: LightProps) {
   return (
     <button
+      disabled={!isPlaying}
       className={clsx(
-        "h-16 w-16 relative overflow-hidden transition-transform active:scale-95 active:brightness-75",
-        className,
+        "h-16 w-16 relative overflow-hidden transition-transform",
         {
           "bg-red-500": isOn,
           "bg-gray-500": !isOn,
-          "cursor-pointer": isPlaying,
+          "cursor-pointer active:scale-95 active:brightness-75": isPlaying,
+          "cursor-default": !isPlaying,
         },
+        className,
       )}
       onClick={onClick}
     >
       <div
         className={clsx(
           "absolute inset-x-0 top-0 h-1/2 bg-gradient-to-b from-white/30 to-transparent pointer-events-none transition-opacity",
-          "active:opacity-50",
+          {
+            "active:opacity-50": isPlaying,
+          },
         )}
       />
     </button>
