18723ad2712c982a84ce92ce1c1e0950a4fa23b3
Leslie Hoare <iam@lesleh.co.uk>
2025-02-21 19:24:58 +0000

    refactor: remove custom webpack configuration for mp3 files
    The custom webpack rule for handling mp3 files has been removed
    from the next.config.js file, simplifying the configuration.

--- next.config.js
+++ next.config.js
@@ -26,17 +26,6 @@ const nextConfig = {
     ],
   },
   reactStrictMode: true,
-  webpack(config, options) {
-    config.module.rules.push({
-      test: /\.(mp3)$/,
-      type: "asset/resource",
-      generator: {
-        filename: "static/chunks/[path][name].[hash][ext]",
-      },
-    });
-
-    return config;
-  },
 };
 
 module.exports = withBundleAnalyzer(nextConfig);
